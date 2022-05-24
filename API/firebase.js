3dd590229298bb396facf114b029143145ea2ddf
Naing Lynn <37519463+nainglynndw@users.noreply.github.com>
2022-05-24 11:19:50 +0630

    Update firebase.js

--- API/firebase.js
+++ API/firebase.js
@@ -9,13 +9,7 @@ import { getFirestore } from "firebase/firestore/lite";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBef-vinGqaxxKYxStyqnSE_JsGaBgekGc",
-  authDomain: "digital-learning-center-saya.firebaseapp.com",
-  projectId: "digital-learning-center-saya",
-  storageBucket: "digital-learning-center-saya.appspot.com",
-  messagingSenderId: "598763712577",
-  appId: "1:598763712577:web:4443cc7b51fb527221d6c0",
-  measurementId: "G-23HJ7M2LKB",
+
 };
 
 // if (firebase.getApps().length === 0) {
